6d0960032bf91a00b3e8e2564d01e8b89f37a6ce
ray <89933907+rayrednet@users.noreply.github.com>
2023-12-25 17:49:46 +0700

    Delete img directory

--- img/routing/Himmel.sh
+++
@@ -1,6 +0,0 @@
-#Routing for Himmel's eth1 paths
- #A8
- route add -net 192.180.14.144 netmask 255.255.255.252 gw 192.180.14.2
-
- #A9
-  route add -net 192.180.14.148 netmask 255.255.255.252 gw 192.180.14.2
